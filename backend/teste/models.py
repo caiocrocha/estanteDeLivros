78dfccb69e8fca591c1c22ab322054be3f3998a1
gabic6 <gabic.iwashima@gmail.com>
2021-11-10 10:57:45 -0300

    comentarios models

--- backend/teste/models.py
+++ backend/teste/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class React(models.Model):
   img = models.CharField(max_length=100) # link da imagem
-  title = models.CharField(max_length=100)
-  author = models.CharField(max_length=100)
+  title = models.CharField(max_length=100) #título do livro
+  author = models.CharField(max_length=100) #autor
